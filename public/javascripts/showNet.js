4fe94535df145ddf8299aa436e908491228f1c20
Helder Ferreira <woounn@gmail.com>
2017-06-28 10:11:38 +0100

    add word to search

--- public/javascripts/showNet.js
+++ public/javascripts/showNet.js
@@ -1,52 +1,53 @@
-
-$.ajax({
-    url: "/Twitter/getDataFromWords",
-    type: "POST",
-    data: {word: "Portugal"},
-    success: function(data) {
-        netInput(data);
-    }
-});
-
-function netInput(data) {
-
-    var idIncremented = 1;
-    var finalArray = [];
-    var finalEdges = [];
-
-    var items = data.map(function(obj) {
-        var newString = obj.text.split(/\s+/);
-        newString.map(function(obj2, index) {
-            if (checkWord(obj2)) {
-
-                var resultofAddPipe = addPipe(obj2,index,idIncremented);
-                if (!(Object.keys(resultofAddPipe).length === 0 && resultofAddPipe.constructor === Object)) {
-                    finalEdges.push(resultofAddPipe);
-                } 
-
-                var auxObj = {};
-                auxObj.id = idIncremented;
-                auxObj.label = obj2;
-                finalArray.push(auxObj);
-
-                idIncremented++
-            }
-        })
+$( "#btn2" ).click(function() {
+    $.ajax({
+        url: "/Twitter/getDataFromWords",
+        type: "POST",
+        data: {word: $('#word').val()},
+        success: function(data) {
+            netInput(data);
+        }
     });
 
-    var nodes = new vis.DataSet(finalArray);
-    var edges = new vis.DataSet(finalEdges);
+    function netInput(data) {
 
-    // create a network
-    var container = document.getElementById('mynetwork');
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    
-    var options = {};
-    var network = new vis.Network(container, data, options);
-}
+        var idIncremented = 1;
+        var finalArray = [];
+        var finalEdges = [];
+
+        var items = data.map(function(obj) {
+            var newString = obj.text.split(/\s+/);
+            newString.map(function(obj2, index) {
+                if (checkWord(obj2)) {
+
+                    var resultofAddPipe = addPipe(obj2,index,idIncremented);
+                    if (!(Object.keys(resultofAddPipe).length === 0 && resultofAddPipe.constructor === Object)) {
+                        finalEdges.push(resultofAddPipe);
+                    } 
+
+                    var auxObj = {};
+                    auxObj.id = idIncremented;
+                    auxObj.label = obj2;
+                    finalArray.push(auxObj);
+
+                    idIncremented++
+                }
+            })
+        });
+
+        var nodes = new vis.DataSet(finalArray);
+        var edges = new vis.DataSet(finalEdges);
+
+        // create a network
+        var container = document.getElementById('mynetwork');
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+        
+        var options = {};
+        var network = new vis.Network(container, data, options);
+    }
+});
 
 function checkWord(word) {
     words = ["por", "da", "a", "de", "por", "to", ".", "and","se", "e", "PM","o", ":", "na", "que", "el"]
--- public/javascripts/showVis.js
+++ public/javascripts/showVis.js
@@ -1,30 +1,32 @@
+$( "#btn1" ).click(function() {
+  
+  $.ajax({
+    url: "/Twitter/getDataFromWords",
+    type: "POST",
+    data: {word: $('#word').val()},
+    success: function(data) {
+      visInput(data);
+    }
+  });
 
-$.ajax({
-  url: "/Twitter/getDataFromWords",
-  type: "POST",
-  data: {word: "Portugal"},
-  success: function(data) {
-    visInput(data);
+  function visInput(data) {
+    var idIncremented = 1;
+    var items = data.map(function(obj) {
+      var auxObj = {};
+      auxObj.id = idIncremented;
+      auxObj.content = "<img src="+obj.user.img+">    "
+      auxObj.content += obj.text;
+      //console.log(auxObj.content);
+      auxObj.start = obj.date;
+      idIncremented++;
+      return auxObj;
+    })
+
+    $('#visualization').html('');
+    
+    var container = document.getElementById('visualization');
+    var options = {autoResize: true};
+    var timeline = new vis.Timeline(container, items, options);
+    
   }
 });
-
-function visInput(data) {
-  var idIncremented = 1;
-  var items = data.map(function(obj) {
-    var auxObj = {};
-    auxObj.id = idIncremented;
-    auxObj.content = "<img src="+obj.user.img+">    "
-    auxObj.content += obj.text;
-    //console.log(auxObj.content);
-    auxObj.start = obj.date;
-    idIncremented++;
-    return auxObj;
-  })
-
-  //console.log(items);
-  
-  var container = document.getElementById('visualization');
-  var options = {autoResize: true};
-  var timeline = new vis.Timeline(container, items, options);
-  
-}
